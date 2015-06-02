37e4bcd9d4088542ef36f79809bdf93ed9cd19e0
Tejas Sharma <sharmatejas0@gmail.com>
2015-06-02 09:28:10 -0400

    multiplication program

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,35 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// R3 stores current computed value
+@R3
+M=0 
+
+(LOOP)
+// Decrement current counter
+@R1
+M=M-1
+D=M
+// If current counter < 0 -> return current computed value
+@RETURN
+D;JLT
+// Else -> add and loop again
+@R0
+D=M
+@R3
+M=M+D
+@LOOP
+0;JMP
+
+(RETURN)
+@R3
+D=M
+@R2
+M=D
+@END
+0;JMP
+
+(END)
+@END
+0;JMP
