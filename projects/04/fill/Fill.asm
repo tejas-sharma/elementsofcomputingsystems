5937167eebedd40bac52aab2b961dce37440f50d
tejas sharma <sharmatejas0@gmail.com>
2015-06-08 22:43:58 -0400

    some ramblings

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,68 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+
+// Start: 16384 
+// End: 147456
+// Every row has 512 bits
+
+// 1 - black
+// 0 - white
+
+// keyboard - 24576
+// no key - 0
+
+// @R1: op
+// BLACKEN = 1
+// WHITEN  = 0
+
+// @R4: prev op
+
+// @R2: Current Bit
+
+// @R3: Remaining bits in row
+
+// Reads a bit into current op register
+(READBIT)
+@24576
+D=M
+@SETWHITE
+D;JEQ
+@R1
+M=1
+(SETWHITE)
+@R1
+M=0
+
+// Checks to see if we will overflow 
+(CHECKNEEDRESET)
+@R1
+D=M
+@R4
+D=D-M
+@RESETWORKINGBIT
+D;JEQ
+@R2
+D=M-147456
+@RESETWORKINGBIT
+D;JLT
+
+// Reset the working bit
+(RESETWORKINGBIT)
+@R2
+M=16384
+@R3
+M=512
+@R1
+D=M
+@R4
+M=D
+
+(PERFORMOP)
+@R1
+D=M
+@WHITEN
+D;JEQ
+
+(WHITEN)
